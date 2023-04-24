4dd9440b7be2bfde33925494ee2e0506f7451064
yashVisavadia <yashvishavadia6996@gmail.com>
2023-04-24 18:21:42 +0530

    Add

---
+++ Last Stone Weight.py
@@ -0,0 +1,17 @@
+from typing import List
+from sortedcontainers import SortedList
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        s = SortedList(stones)
+        while len(s) > 1:
+            y = s.pop()
+            x = s.pop()
+
+            if x != y:
+                y -= x
+                s.add(y)
+        if s:
+            return s[0]
+        return 0
