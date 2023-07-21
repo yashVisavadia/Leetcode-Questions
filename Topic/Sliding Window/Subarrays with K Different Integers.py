c0f9d2172f8dfec938dd160825fce7d7e43e6d42
yashVisavadia <yashvishavadia6996@gmail.com>
2023-07-21 20:31:30 +0530

    Modified .gitignore

---
+++ Topic/Sliding Window/Subarrays with K Different Integers.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def subarraysWithKDistinct(self, A: List[int], k: int) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1, 2, 1, 2, 3]
+    k = 2
+    print(sol.subarraysWithKDistinct(nums))
